bf2f1ab23725f035e9e35e06364db5a1c0f59440
Ronan Pardo Soares <ronan.soares@gmail.com>
2023-05-15 15:59:05 -0300

    Fix change of tests

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ cond3 = False
 cond4 = False
 
 if numero < 0:
-  print("impossível!")
+  print("Impossível!")
   cond1 = True
 if numero < 18:
-  print("não precisa se alistar.")
+  print("Não precisa se alistar.")
   cond2 = True
 if 18 < numero < 65:
   print("Não esqueça de votar na próxima eleição.")
@@ -18,4 +18,4 @@ if numero > 65:
   print("Vá descansar.")
   cond4 = True
 if not (cond1 or cond2 or cond3 or cond4):
-  print("eita!")
+  print("Eita!")
